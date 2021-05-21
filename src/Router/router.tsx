b3badf34f80e433e6fb0599b0d14d81a64029cb4
buitranduytuan <btdt94@gmail.com>
2021-05-21 23:55:52 +0700

    Test

--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -1,32 +1,31 @@
-import React from "react";
-import {
-    Switch,
-    Route,
-} from "react-router-dom";
-import ListProduct from "../Components/Products/listProduct";
-import Home from "../Components/Home/home";
-import Checkout from "../Components/Checkout/checkout";
-import ProductDetail from "../Components/ProductDetail/productDetail";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import ListProduct from '../Components/Products/listProduct';
+import Home from '../Components/Home/home';
+import Checkout from '../Components/Checkout/checkout';
+import ProductDetail from '../Components/ProductDetail/productDetail';
+import FormExample from '../Components/Form/formExample';
 
 const AppRouter = () => {
-    return (
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route exact path="/checkout">
-                <Checkout />
-            </Route>
-            <Route exact path="/products">
-                <ListProduct />
-            </Route>
-            <Route path="/productdetail">
-                <ProductDetail />
-            </Route>
-        </Switch>
-    );
-
-}
-
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/checkout">
+        <Checkout />
+      </Route>
+      <Route exact path="/products">
+        <ListProduct />
+      </Route>
+      <Route path="/productdetail">
+        <ProductDetail />
+      </Route>
+      <Route path="/formExample">
+        <FormExample />
+      </Route>
+    </Switch>
+  );
+};
 
 export default AppRouter;
